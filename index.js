fb4417408ffe2e0712deac210853bb2249826f57
priscillahuynh <priscillamhuynh@gmail.com>
2021-03-26 08:23:49 +0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driverName) {
+      return driverName.toLowerCase() === name.toLowerCase();
+    });
+  }
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(function(driverName) {
+        return driverName.slice(0, letter.length) === letter;
+    })
+}
+
+function matchName(list, name){
+    return list.filter(function(driverName) {
+        return driverName.name.toLowerCase() === name.toLowerCase();
+    })
+}
